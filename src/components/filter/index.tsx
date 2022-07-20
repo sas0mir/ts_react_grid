7a97e5872008920c8d2920faf514c6ecb7080f2d
Станислав Смирнов <stanislavsmirnov@Stanislavs-MacBook-Pro.local>
2022-07-21 00:12:37 +0200

    filter types and styles

--- src/components/filter/index.tsx
+++ src/components/filter/index.tsx
@@ -13,12 +13,21 @@ const Filter: FC<FilterProps> = (props) => {
 
     const { title, data, value, change, type } = props;
 
-    console.log('TYPE->', type, title);
+    console.log('TYPE->', type === 'object', type, title);
     
   return (
     <div className={classes.component_filter_container}>
-      <label htmlFor={title}>{title}</label>
-      <input type={type === 'object' ? 'file' : type} id={title} placeholder={value} onChange={(e) => change(title, e.target.value)}></input>
+      <label htmlFor={title} className={classes.component_filter_label}>{title}</label>
+      {type !== 'object' ?
+      <input type={type} id={title} placeholder={value} onChange={(e) => change(title, e.target.value)} className={classes.component_filter_input}></input> :
+      <select name={title} id={title} form={this} onSelect={(e) => change(title, e)} className={classes.component_filter_select}>
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="opel">Opel</option>
+          <option value="audi">Audi</option>
+        </select>
+      }
+      
     </div>
   );
 }
